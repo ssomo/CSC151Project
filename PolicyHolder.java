a20472df3a5d2dfead2f757abc20f6161c5d36b7
ssomo <78325033+ssomo@users.noreply.github.com>
2021-04-13 14:20:14 -0400

    Update PolicyHolder.java
    Moved fields and methods to the PolicyHolder class

---
+++ PolicyHolder.java
@@ -0,0 +1,27 @@
+//Create a policy holder class that will represent a person that 
+//is associated with an insurance policy.
+
+//Move the appropriate fields and methods of the Policy class into the PolicyHolder class 
+//(Hint: the fields and methods you move into the PolicyHolder class should be directly associated with a policy holder).
+
+//Add a toString method to both the Policy and PolicyHolder classes.
+
+//Add a static field that is used to keep track of the number of Policy objects that have been created.
+
+//Demonstrate proper use of class collaboration in such a way that a Policy has a PolicyHolder.
+
+//Alter the Demo class to implicitly call the toString method you created in Step 2 when outputting information about the Policy objects.
+
+//The demo program should display the number of Policy objects that were created, the number of Policyholders that are smokers, and the number of Policyholders that are non-smokers.
+public class PolicyHolder //aggregate class
+{
+   //declare fields
+   private String policyNumber, providerName, firstName, lastName;
+   private double weight, height;
+   private int age;
+   
+   
+   
+   
+   
+}
